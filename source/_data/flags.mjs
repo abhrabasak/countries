2cc156690139dcf32928b9a820093b57d1797b1f
Abhra Basak <abhra@outlook.in>
2025-02-16 01:31:15 -0800

    Process SVG to fixed width

--- source/_data/flags.mjs
+++ source/_data/flags.mjs
@@ -1,5 +1,16 @@
 import fs from "fs";
 import path from "path";
+import { DOMParser, XMLSerializer } from "xmldom";
+
+const processSvg = (originalSvg, width) => {
+  const doc = new DOMParser().parseFromString(originalSvg, "image/svg+xml");
+  const svg = doc.documentElement;
+  const [x, y, w, h] = svg.getAttribute("viewBox").split(" ").map(Number);
+
+  svg.setAttribute("width", `${width}`);
+  svg.setAttribute("height", `${(width * h) / w}`);
+  return new XMLSerializer().serializeToString(doc);
+};
 
 const svgs = (dir) =>
   fs
@@ -7,7 +18,7 @@ const svgs = (dir) =>
     .filter((f) => path.extname(f) === ".svg")
     .map((f) => ({
       k: path.parse(f).name,
-      v: fs.readFileSync(path.join(dir, f), "utf8"),
+      v: processSvg(fs.readFileSync(path.join(dir, f), "utf8"), 200),
     }))
     .reduce((a, { k, v }) => {
       a[k] = v;
