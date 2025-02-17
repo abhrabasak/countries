622f7e2303693d1c442d83fc62cb4c1baf3f2b54
Abhra Basak <abhra@outlook.in>
2025-02-16 22:59:38 -0800

    Adjust directives for TW4

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,1 @@
-// export default { plugins: { "@tailwindcss/postcss": {} } };
-
-module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer")],
-};
+export default { plugins: { "@tailwindcss/postcss": {} } };
